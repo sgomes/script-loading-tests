2e5c5d872507f8704dfd3f58e808cfbc0d188efd
Sérgio Gomes <mail@sgomes.com>
2023-05-05 15:11:40 +0100

    Add tests

---
+++ slow.js
@@ -0,0 +1,11 @@
+console.log('Started slow script execution');
+performance.mark('script-start');
+function fib(x) {
+  if (x <= 0) return 0;
+  if (x == 1) return 1;
+  return fib(x-1) + fib(x-2);
+}
+fib(41);
+performance.mark('script-end');
+performance.measure('script-run', 'script-start', 'script-end');
+console.log('Finished slow script execution');
